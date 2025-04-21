c5cd56bd5ac64416635e29bad1f10a946a0612d7
hos-prog <hosamaadem8@gmail.com>
2025-04-21 22:52:03 +0300

    Create calculator.py
    # Calculator Project 🧮
    
    A simple calculator app built using Python. It supports basic operations like addition, subtraction, multiplication, and division. The user can continue calculating with the result or start a new calculation.
    
    ## Features
    - Addition, Subtraction, Multiplication, Division
    - User-friendly command-line interface
    - Looping calculation feature
    - Error handling for division by zero
    
    ## How to Run
    Make sure Python is installed. Then run:
    
    ```bash
    python calculator.py

---
+++ calculator.py
@@ -0,0 +1,48 @@
+# AdemHosama - Calculator
+# April 21/2025 GC.
+from art import logo
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n2 == 0:
+        return "Error: Division by zero"
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+def calculator():
+    print(logo)
+    num1 = float(input("Enter the first number: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("Enter the next number: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        next_step = input(f"Type 'y' to continue calculating with {answer}, or 'n' to start a new one: ").lower()
+        if next_step == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
